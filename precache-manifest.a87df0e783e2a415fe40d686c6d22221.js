14685c38df953848eb3d5f0a2ba4b6a2a7f6b449
Roman Fedusevych <romanfyks@gmail.com>
2021-08-30 21:13:51 +0300

    Updates

---
+++ precache-manifest.a87df0e783e2a415fe40d686c6d22221.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "937df80b0f45a5a80a439844f36edb52",
+    "url": "./index.html"
+  },
+  {
+    "revision": "896a9943f68cd90dd06e",
+    "url": "./static/css/main.68fff2fd.chunk.css"
+  },
+  {
+    "revision": "bef14ca31350064182c6",
+    "url": "./static/js/2.e8d9cca5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.e8d9cca5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "896a9943f68cd90dd06e",
+    "url": "./static/js/main.fa11d699.chunk.js"
+  },
+  {
+    "revision": "e447b2ddaa3314191b11",
+    "url": "./static/js/runtime-main.e90a3edd.js"
+  }
+]);
